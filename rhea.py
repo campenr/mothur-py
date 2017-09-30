a21896edff4ef98aec67834df4fed956dda197f0
Richard Campen <richard@campen.co>
2017-09-30 16:35:18 -0400

    general code tidy up

--- rhea.py
+++ rhea.py
@@ -1,3 +1,12 @@
+"""
+Copyright (c) 2017, Richard Campen
+All rights reserved.
+
+Licensed under the Modified BSD License.
+For full license terms see LICENSE.txt
+
+"""
+
 from subprocess import PIPE, Popen, STDOUT
 import random
 import os
@@ -40,7 +49,7 @@ class MothurCommand:
     Callable handler for mothur function calls generated from unknown method calls for `mothur`.
 
     ..note:: This class will call itself iteratively if passed an attribute until it is passed a callable
-    at which point it with format the function call for mothur and execute it using mothur's batch mode.
+    at which point it will format the function call for mothur and execute it using mothur's command line mode.
 
     """
 
@@ -52,13 +61,10 @@ class MothurCommand:
         :param command_name: the name of this class instance
         :type command_name: str
 
-        ..note: MothurCommand attributes are preceded with an underscore to prevent accidental collision with actual
-            mothur commands.
-
         """
 
-        self._root = root
-        self._command_name = command_name
+        self.root_object = root
+        self.command_name = command_name
 
     def __getattr__(self, command_name):
         """
@@ -66,29 +72,41 @@ class MothurCommand:
         :param command_name: the name of the attribute being requested by the calling function/class
         :type command_name: str
 
-        :return: a new MothurCommand instance with self.parent set to this MothurCommand instance.
-        :returns: rhea.core.MothurCommand
-
         ..warning: if the command_name is not a valid mothur command mothur will error.
 
         """
 
-        return MothurCommand(self._root, '%s.%s' % (self._command_name, command_name))
+        return MothurCommand(self.root_object, '%s.%s' % (self.command_name, command_name))
 
     def __repr__(self):
-        return 'MothurCommand(root=<%s object>, name=%r)' % (self._root, self._command_name)
+        return 'MothurCommand(root=<%s object>, name=%r)' % (self.root_object, self.command_name)
 
     def __str__(self):
-        return '%s.%s' % (self._root, self._command_name)
+        return '%s.%s' % (self.root_object, self.command_name)
 
     def __call__(self, *args, **kwargs):
-        """
-        Catches method calls and formats and executes them as commands within mothur.
+        """Catches method calls and formats and executes them as commands within mothur."""
 
-        """
+        # --------------- initialise variables --------------- #
 
-        # extract name of mothur command
-        mothur_command = self._command_name
+        # output flags
+        user_input_flag = False
+        mothur_warning_flag = False
+        mothur_error_flag = False
+
+        # parsing flags
+        parse_current_flag = False
+
+        # dict containing strings to find in lines, with matching current_dirs keys
+        # mothur prints out the current directory for each category on the same line at the matched string
+        current_dir_headers = {
+            'Current input directory saved by mothur:': 'input',
+            'Current output directory saved by mothur:': 'output',
+            'Current default directory saved by mothur:': 'tempdefault'
+        }
+        current_dir_keys = current_dir_headers.keys()
+
+        # --------------- format mothur input --------------- #
 
         # get and format arguments for mothur command call
         formatted_args = ''
@@ -109,17 +127,18 @@ class MothurCommand:
 
         # create commands
         commands = list()
-        base_command = '{0}({1})'.format(mothur_command, mothur_args)
+        base_command = '{0}({1})'.format(self.command_name, mothur_args)
         commands.append(base_command)
-        if self._root.current_files:
-            current_files = ', '.join(['%s=%s' % (k, v) for k, v in self._root.current_files.items()])
+        if self.root_object.current_files:
+            current_files = ', '.join(['%s=%s' % (k, v) for k, v in self.root_object.current_files.items()])
             commands.insert(0, 'set.current(%s)' % current_files)
-        if self._root.current_dirs:
-            current_dirs = ', '.join(['%s=%s' % (k, v) for k, v in self._root.current_dirs.items()])
+        if self.root_object.current_dirs:
+            current_dirs = ', '.join(['%s=%s' % (k, v) for k, v in self.root_object.current_dirs.items()])
             commands.insert(0, 'set.dir(%s)' % current_dirs)
         commands.append('get.current()')
 
-        if self._root.suppress_logfile:
+        # create logfile name
+        if self.root_object.suppress_logfile:
             # force name of mothur logfile
             logfile = 'mothur.rhea.logfile'
         else:
@@ -129,7 +148,7 @@ class MothurCommand:
                 rn = random.randint(10000, 99999)
                 logfile_path = 'mothur.rhea.%d.logfile' % rn
 
-                out_dir = self._root.current_dirs.get('output', False)
+                out_dir = self.root_object.current_dirs.get('output', False)
                 if out_dir:
                     logfile_path = os.path.join(out_dir, logfile_path)
                 if not os.path.isfile(logfile_path):
@@ -137,41 +156,24 @@ class MothurCommand:
                     break
         commands.insert(0, 'set.logfile(name=%s)' % logfile)
 
-        # format commands for mothur execution
+        # combine commands for mothur execution
         commands_str = '; '.join(commands)
 
-        # output flags
-        user_input_flag = False
-        mothur_error_flag = False
-
-        # setup byte encoded strings for conditional printing
-        base_command_bytes = base_command
-        get_current_bytes = 'mothur > get.current()'
-        mothur_warning_bytes = '<<<'
-        mothur_error_bytes = '***'
-
-        # parsing flags
-        parse_current_flag = False
-
-        # setup byte encoded strings for conditional output parsing
-        current_dir_bytes = {
-            'Current input directory saved by mothur:': 'input',
-            'Current output directory saved by mothur:': 'output',
-            'Current default directory saved by mothur:': 'tempdefault'
-        }
-        current_dir_bytes_keys = current_dir_bytes.keys()
-        current_files_bytes = 'Current files saved by mothur:'
+        # --------------- run mothur --------------- #
 
         # run mothur command in command line mode
+        # TODO: add support for variable mothur executable locations
         p = Popen(['mothur', '#%s' % commands_str], stdout=PIPE, stderr=STDOUT)
 
         try:
             with p.stdout:
                 for line in iter(p.stdout.readline, b''):
 
-                    # check for valid verbosity setting
-                    if 0 <= self._root.verbosity < 3:
+                    # check for valid verbosity
+                    if not(0 <= self.root_object.verbosity < 3):
+                        raise (ValueError('verbosity must be 0, 1, or 2.'))
 
+                    else:
                         # strip newline characters as print statement will insert its own
                         line = line.replace(b'\r', b'')
                         line = line.rsplit(b'\n')[0]
@@ -180,80 +182,78 @@ class MothurCommand:
                         line = line.decode()
 
                         # only print output if verbosity not zero
-                        if self._root.verbosity > 0:
+                        if self.root_object.verbosity > 0:
 
-                            # conditionally set user_input_flag
-                            if base_command_bytes in line:
+                            # conditionally set flags
+                            # user input spans output from the base command until the get.current() command
+                            if base_command in line:
                                 user_input_flag = True
-                            elif get_current_bytes in line:
+                            elif 'mothur > get.current()' in line:
                                 user_input_flag = False
 
-                            # conditionally set mothur_error_flag
-                            if mothur_warning_bytes in line or mothur_error_bytes in line:
+                            # mothur prints warning messages starting with a string containing '<<<'
+                            if '<<<' in line:
+                                mothur_warning_flag = True
+
+                            # mothur prints error messages starting with a string containing '***'
+                            if '***' in line:
                                 mothur_error_flag = True
 
-                        if self._root.verbosity == 1:
-                            if any([user_input_flag, mothur_error_flag]):
+                        # conditionally print output based on flags
+                        if self.root_object.verbosity == 1:
+                            if any([user_input_flag, mothur_warning_flag, mothur_error_flag]):
                                 print(line)
-                                # print(line.decode())
-                        elif self._root.verbosity == 2:
+                        elif self.root_object.verbosity == 2:
                             print(line)
-                            # print(line.decode())
 
                         # check for current dirs
-                        for key in current_dir_bytes_keys:
+                        for key in current_dir_keys:
                             if key in line:
                                 current_dir = line.split(' ')[-1].split('\n')[0]
-                                self._root.current_dirs[current_dir_bytes[key]] = current_dir
+                                self.root_object.current_dirs[current_dir_headers[key]] = current_dir
 
                         # conditionally reset flag for parsing current files from stdout
+                        # mothur prints a blank line after the list of current files
                         if line == '':
                             parse_current_flag = False
 
-                        # save current files while parse flag is true
+                        # conditionally parse current files from stdout
                         if parse_current_flag:
                             current_file = line.split('=')
                             current_file_type = current_file[0]
                             current_file_name = current_file[1]
-                            self._root.current_files[current_file_type] = current_file_name
+                            self.root_object.current_files[current_file_type] = current_file_name
 
                         # check for current files
-                        if current_files_bytes in line:
+                        # mothur prints out the current files after the line containing 'Current files saved by mothur:'
+                        if 'Current files saved by mothur:' in line:
                             parse_current_flag = True
 
-                    else:
-                        raise(ValueError('verbosity must be 0, 1, or 2.'))
-
-            return_code = p.wait()  # wait for the subprocess to finish
-            if return_code != 0:
-                raise(MothurError('Mothur encounted an error.'))
+            # wait for the subprocess to finish then check for erroneous output or return code
+            return_code = p.wait()
+            if return_code != 0 or mothur_error_flag:
+                raise(RuntimeError('Mothur encounted an error with return_code=%s and mothur_error_flag=%s' %
+                                   (return_code, mothur_error_flag)))
 
         except KeyboardInterrupt:
             # tidy up running process before raising exception when keyboard interrupt detected
-            p.terminate()
-            raise(KeyboardInterrupt('User terminated process.'))
-
-        except MothurError as e:
-            raise e
+            # TODO: need a better way to kill the process on windows.
+            p.kill()
+            raise(KeyboardInterrupt('User terminated the process.'))
 
         finally:
             # conditionally cleanup logfile
-            if self._root.suppress_logfile is True:
-                # need to append output directory to logfile path if it has been set else
-                out_dir = self._root.current_dirs.get('output', False)
+            if self.root_object.suppress_logfile is True:
+                # need to append output directory to logfile path if it has been set else we won't be able to find it
+                out_dir = self.root_object.current_dirs.get('output', False)
                 if out_dir:
                     logfile = os.path.join(out_dir, logfile)
 
                 # TODO: Mothur only renames the logfile to something predictable if exits properly, else this will fail
+                # TODO: see https://github.com/mothur/mothur/issues/281 and https://github.com/mothur/mothur/issues/377
                 try:
                     os.remove(logfile)
                 except FileNotFoundError:
                     print('[WARNING]: could not delete mothur logfile')
 
-            return self._root
-
-
-class MothurError(Exception):
-
-    def __init__(self, *args, **kwargs):
-        pass
+            return self.root_object
