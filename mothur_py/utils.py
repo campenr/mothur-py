5d3fcd7033ccfa7338b1ef1211d2ba1f692a5843
Richard Campen <richard@campen.co>
2018-02-22 16:19:41 -0500

    Remove tidy_output_dir function and put in gist instead

--- mothur_py/utils.py
+++ mothur_py/utils.py
@@ -8,30 +8,7 @@ For full license terms see LICENSE.txt
 """
 
 import collections
-import os
 
-
-def tidy_output_dir(mothur_obj, whitelist=list()):
-    """Removes files from the output directory that are not listed in the current_files attribute or a whitelist."""
-
-    # append current files to whitelist
-    for file in mothur_obj.current_files.values():
-        whitelist.append(file)
-
-    # tidy up environment to help save space
-    out_dir = mothur_obj.current_dirs.get('output', None)
-
-    # purge non-whitelist files from the output directory
-    for file in os.listdir(out_dir):
-
-        # conditionally correct filepath
-        if out_dir is not None:
-            file = os.path.join(out_dir, file)
-
-        # only process files, not directories
-        if os.path.isfile(file):
-            if file not in whitelist:
-                os.remove(file)
 
 def format_mothur_params(*args, **kwargs):
     """Formats mothur command paramters from python variables into a string formatted for mothur."""
